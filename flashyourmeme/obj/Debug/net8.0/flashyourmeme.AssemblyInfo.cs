5bec97800de7634579cbc2237fcf699f3476c31d
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-07-23 01:14:17 -0600

    Update Display Setting Page

--- flashyourmeme/obj/Debug/net8.0/flashyourmeme.AssemblyInfo.cs
+++ flashyourmeme/obj/Debug/net8.0/flashyourmeme.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("flashyourmeme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ad818ca51acafcf2150b61d961d2250611193e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6273d6b009da862402d8797221d94d3215ce7072")]
 [assembly: System.Reflection.AssemblyProductAttribute("flashyourmeme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("flashyourmeme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
