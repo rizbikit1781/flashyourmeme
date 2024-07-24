9b38a114de92f451b524ab67e1266e4d36b31401
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-07-23 23:55:57 -0600

    Update Bar and Progress Chart

--- flashyourmeme/obj/Debug/net8.0/flashyourmeme.AssemblyInfo.cs
+++ flashyourmeme/obj/Debug/net8.0/flashyourmeme.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("flashyourmeme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6273d6b009da862402d8797221d94d3215ce7072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bec97800de7634579cbc2237fcf699f3476c31d")]
 [assembly: System.Reflection.AssemblyProductAttribute("flashyourmeme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("flashyourmeme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
