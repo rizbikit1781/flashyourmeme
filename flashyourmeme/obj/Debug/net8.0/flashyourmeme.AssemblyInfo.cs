d8d485e7d75413db244316b8f52e5a78662669ed
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-07-28 01:45:21 -0600

    Update Branch Management

--- flashyourmeme/obj/Debug/net8.0/flashyourmeme.AssemblyInfo.cs
+++ flashyourmeme/obj/Debug/net8.0/flashyourmeme.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("flashyourmeme")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2504498ce3f3e08ea52c876d6d33349aaee24d88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1192f1b4f5f86d364bc90c00783c63bfd4403952")]
 [assembly: System.Reflection.AssemblyProductAttribute("flashyourmeme")]
 [assembly: System.Reflection.AssemblyTitleAttribute("flashyourmeme")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
